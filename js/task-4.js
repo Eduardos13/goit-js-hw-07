d29bd0aa58ec92a40a7b68c9915994d395387e47
Eduardos13 <edik.kozelsky@gmail.com>
2024-04-26 20:54:30 -0400

    task 4 is done
    updated css for some prev. tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,32 @@
+const form = document.querySelector(".login-form")
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit (event) {
+    event.preventDefault();
+
+    let texValue = true;
+
+    const inputs = document.querySelectorAll("input");
+
+    inputs.forEach(input => {
+        if(!input.value.trim()) {
+            texValue = false;
+        }
+    });
+
+    if (!texValue) {
+        alert ("All form fields must be filled in");
+    }
+
+    const inputInfo = {};
+
+    form.querySelectorAll("input").forEach(input => {
+        inputInfo[input.name] = input.value.trim()
+    })
+
+    console.log(inputInfo);
+
+    event.target.reset();
+}
 
